122a7771f084ba59b478cf3c7b93f6ec8f9007e9
judmitriev <judmitriev@ecocompany.ru>
2020-11-13 04:37:07 +0300

    overrided equals() and hashcode() for triangles

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import model.Shape;
 import model.triangle.EquilateralTriangle;
 import model.triangle.IsoscelesTriangle;
 import model.triangle.RectangularTriangle;
@@ -6,13 +5,13 @@ import model.triangle.Triangle;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle[] triangles = new  Triangle[4];
-        triangles[0] = new Triangle(4,5,6);
-        triangles[1] = new IsoscelesTriangle(5,4);
+        Triangle[] triangles = new Triangle[4];
+        triangles[0] = new Triangle(4, 5, 6);
+        triangles[1] = new IsoscelesTriangle(5, 4);
         triangles[2] = new EquilateralTriangle(3);
         triangles[3] = new RectangularTriangle(3, 4);
         for (Triangle triangle : triangles) {
-            System.out.println(triangle);
+            System.out.println(triangle + "\n");
         }
     }
 }
--- src/main/java/model/triangle/Triangle.java
+++ src/main/java/model/triangle/Triangle.java
@@ -2,6 +2,8 @@ package model.triangle;
 
 import model.Shape;
 
+import java.util.Objects;
+
 public class Triangle extends Shape {
 
     double firstSide;
@@ -40,5 +42,20 @@ public class Triangle extends Shape {
         builder.append("\nПериметр: ").append(getPerimeter());
         builder.append("\nПлощадь: ").append(getSquare());
         return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        if (Double.compare(triangle.firstSide, firstSide) != 0) return false;
+        if (Double.compare(triangle.secondSide, secondSide) != 0) return false;
+        return Double.compare(triangle.thirdSide, thirdSide) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstSide, secondSide, thirdSide);
     }
 }
